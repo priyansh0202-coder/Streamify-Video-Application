c2482b7e83daa0a15893963975445bcee0260708
priyansh0202-coder <priyanshsoni36@gmail.com>
2024-03-23 17:28:29 +0530

    bug Fix

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
+  method: 'GET',
+  url: '',
   params: {
-    maxResults: 50,
+    maxResults: '50'
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-  },
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
 };
 
 export const fetchFromAPI = async (url) => {
